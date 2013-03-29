57acf396d2c97549b2b5efe2b7f378821c493487
Alex <bigbadmath@gmail.com>
2013-03-29 19:26:06 -0400

    With image upload, single

--- transbucket/app/models/pin.rb
+++ transbucket/app/models/pin.rb
@@ -1,9 +1,13 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :image
 
   validates :description, presence: true
+  validates :user_id, presence: true
+  validates_attachment :image, presence: true,
+  			content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
+ 			 size: {less_than: 6.megabytes }
   
 
   belongs_to :user
-  validates :user_id, presence: true
+  has_attached_file :image
 end
--- transbucket/db/schema.rb
+++ transbucket/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328031252) do
+ActiveRecord::Schema.define(:version => 20130328035816) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "pins", ["user_id"], :name => "index_pins_on_user_id"

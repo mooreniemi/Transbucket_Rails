3f03e3f77216ecdd7ace825b6119dc47e8d1ae5b
Alex <bigbadmath@gmail.com>
2013-03-29 21:02:51 -0400

    Added jquery masonry

--- transbucket/app/assets/javascripts/application.js
+++ transbucket/app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.masonry.min.js
 //= require bootstrap
 //= require_tree .
--- transbucket/app/controllers/pins_controller.rb
+++ transbucket/app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ before_filter :authenticate_user!, except: [:index]
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all
+    @pins = Pin.order("created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb

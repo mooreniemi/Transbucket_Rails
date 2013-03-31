ce74f79ef742cbbe8fd97915634f9038ca36fe49
Alex <bigbadmath@gmail.com>
2013-03-31 18:03:57 -0400

    Switched to mysql database

--- transbucket/app/models/pin.rb
+++ transbucket/app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
   has_many :pin_images, :dependent => :destroy
-  attr_accessible :description, :pin_images, :pin_images_attributes, :surgeon, :cost, :revision, :details
+  attr_accessible :description, :pin_images, :pin_images_attributes, :surgeon, :cost, :revision, :details, :procedure
 
   accepts_nested_attributes_for :pin_images #, :reject_if => lambda { |t| t['pin_image'].nil? }
 

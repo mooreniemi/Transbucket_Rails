c7e7f4d97b6f86e0003f1bb2bb6b0375ce9eeaa0
Alex <bigbadmath@gmail.com>
2013-05-26 17:36:19 -0400

    Ajaxifying comments, updating search style

--- app/assets/javascripts/pins.js.coffee
+++ app/assets/javascripts/pins.js.coffee
@@ -2,6 +2,6 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-jQuery ->
-	$('#pins').imageLoaded ->
-	$('#pins').masonry itemSelector: ".box"
+#jQuery ->
+#	$('#pins').imageLoaded ->
+#	$('#pins').masonry itemSelector: ".box"
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,19 @@
 # comments_controller.rb
     class CommentsController < ApplicationController
-
-      def create
+def create
         @comment_hash = params[:comment]
         @obj = @comment_hash[:commentable_type].constantize.find(@comment_hash[:commentable_id])
         # Not implemented: check to see whether the user has permission to create a comment on this object
         @comment = Comment.build_from(@obj, current_user, @comment_hash[:body])
         if @comment.save
-          render :html => "comments/comment", :locals => { :comment => @comment }, :layout => false, :status => :created
+          render :partial => "comments/comment", :locals => { :comment => @comment }, :layout => false, :status => :created
         else
           render :js => "alert('error saving comment');"
         end
       end
       def destroy
       @comment = Comment.find(params[:id])
+      sleep 5
       if @comment.destroy
         render :json => @comment, :status => :ok
       else

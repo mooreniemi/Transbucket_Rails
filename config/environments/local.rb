68edb801cc143db9a7f10735a140560eacfe5973
Alex Moore-Niemi <alex@promoboxx.com>
2013-10-14 14:18:53 -0400

    More s3 creds

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -1,6 +1,15 @@
 Transbucket::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.

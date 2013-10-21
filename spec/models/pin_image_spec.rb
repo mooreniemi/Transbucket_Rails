1c10858591f085962593a44b9b059acd23a52eae
Alex Moore-Niemi <alex@promoboxx.com>
2013-10-20 23:54:54 -0400

    Factory girl included in rspec

--- spec/models/pin_image_spec.rb
+++ spec/models/pin_image_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe PinImage do
 	it "has a valid factory" do
-		expect(FactoryGirl.create(:pin_images)).to be_valid
+		expect(FactoryGirl.create(:pin_image)).to be_valid
 	end
 	it "has attached photo" do
-		expect(FactoryGirl.create(:pin_images).should_receive(:photo)) #.and_return(true)
+		expect(FactoryGirl.create(:pin_image).should_receive(:photo)) #.and_return(true)
 	end
 end

2f1683dcd84c80219dd65d4f488b3e8a9247bf0b
Alex Moore-Niemi <alex@promoboxx.com>
2013-10-21 20:36:36 -0400

    fix for default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Transbucket::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "www.transbucket.com" }
 
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",

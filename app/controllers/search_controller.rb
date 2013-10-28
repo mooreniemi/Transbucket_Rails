5f333757ea1d217fdfeed037935e23d60d32a0d4
Alex Moore-Niemi <alex@promoboxx.com>
2013-10-27 20:19:29 -0400

    precompile

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,6 @@ respond_to :json
   def search_terms
     @terms = (Pin.all.map(&:procedure).compact.reject(&:blank?) + Pin.all.map(&:surgeon).compact.reject(&:blank?)).uniq
     @terms.each {|term| term.gsub!(/[\W]/, ' ')}
-    render :json => @terms
+    render :json => {suggestions: @terms}
   end
 end

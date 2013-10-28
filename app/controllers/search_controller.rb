1cf6a3686dc7ff9141acd46eab6782f186d69904
Alex Moore-Niemi <alex@promoboxx.com>
2013-10-27 23:42:31 -0400

    Real search, finally

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,18 @@ respond_to :json
   def search_terms
     @terms = (Pin.all.map(&:procedure).compact.reject(&:blank?) + Pin.all.map(&:surgeon).compact.reject(&:blank?)).uniq
     @terms.each {|term| term.gsub!(/[\W]/, ' ')}
-    render :json => @terms
+    @matches = []
+    @term = params[:term]
+    @terms.each {|t| @matches << t if /(#{@term})/.match(t.downcase)}
+    render :json => @matches
+  end
+
+  def surgeons_only
+    @terms = (Pin.all.map(&:surgeon).compact.reject(&:blank?)).uniq
+    @terms.each {|term| term.gsub!(/[\W]/, ' ')}
+    @matches = []
+    @term = params[:term]
+    @terms.each {|t| @matches << t if /(#{@term})/.match(t.downcase)}
+    render :json => @matches
   end
 end

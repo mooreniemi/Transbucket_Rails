8bc91643d2be4759c537546ce258eaa5c8b7cfc3
Alex Moore-Niemi <alex@promoboxx.com>
2013-11-06 16:42:21 -0500

    comment service object and pin presenter spec

---
+++ app/services/comment_service.rb
@@ -0,0 +1,26 @@
+#app/services/comments_service.rb
+class CommentService
+  attr_reader :body, :commentable, :user
+
+  def initialize(commentable, user, body)
+    @commentable = commentable
+    @body = body
+    @user = user
+  end
+
+  def create
+    comment = Comment.build_from(commentable, user, body)
+    comment
+  end
+
+  def create_and_notify
+    comment = create(commentable, user, body)
+    send_email_notification(comment)
+    comment
+  end
+
+  def send_email_notification(comment)
+    CommentMailer.new_comment_email(self).deliver
+  end
+
+end
---
+++ spec/services/comment_service_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe CommentService do
+  it "can create a comment" do
+
+  end
+
+  it "can create a comment and notify the submission owner" do
+
+  end
+end

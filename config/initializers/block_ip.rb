15adbef1122e19d88f7efe2699d11d66d737d7fe
Alex Moore-Niemi <alex@promoboxx.com>
2013-11-16 18:41:54 -0500

    blocking ip

--- config/initializers/block_ip.rb
+++ config/initializers/block_ip.rb
@@ -1,7 +1,11 @@
-use Rack::Block do
-  ip_pattern '142.157.201.53' do
-  # expressions like '192.0.0.' also available
-    halt 404
+Transbucket::Application.configure do
+
+  config.middleware.insert_before(Rack::Lock, Rack::Block) do
+    # Add your rules with the rack-block syntax in here
+      ip_pattern '142.157.201.53' do
+      # expressions like '192.0.0.' also available
+        halt 404
+      end
   end
+
 end
-run App.new

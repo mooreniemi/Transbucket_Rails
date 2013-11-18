21495a158362579f518c030bfe9599564831297b
Alex Moore-Niemi <alex@promoboxx.com>
2013-11-17 20:20:09 -0500

    by_user filtering

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -22,6 +22,11 @@ before_filter :authenticate_user!, except: [:index]
     end
   end
 
+  def by_user
+    @presenter = PinPresenter.new(user: current_user)
+    @pins = @presenter.by_user(User.find(params[:by_user]))
+  end
+
   # GET /pins/1
   # GET /pins/1.json
   def show

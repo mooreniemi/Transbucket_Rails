3b46ba1fc7072c4d3f8559da544c409cb41edb6a
Alex Moore-Niemi <alex@promoboxx.com>
2013-12-01 11:51:33 -0500

    organizing rake tasks

--- app/helpers/constants.rb
+++ app/helpers/constants.rb
@@ -1,15 +1,15 @@
 module Constants
   FTM = ["phalloplasty", "periareolar mastectomy (keyhole)", "double incision without grafts", "double incision with grafts", "metoidioplasty", "t anchor double incision"]
-  #FTM_IDS = FTM.collect! {|e| Procedure.find_by_name(e).id}
+  FTM_IDS = FTM.collect! {|e| Procedure.find_by_name(e).id} unless Procedure.all.empty?
 
   MTF = ["vaginoplasty", "breast augmentation", "facial feminization surgery"]
-  #MTF_IDS = MTF.collect! {|e| Procedure.find_by_name(e).id}
+  MTF_IDS = MTF.collect! {|e| Procedure.find_by_name(e).id} unless Procedure.all.empty?
 
   TOP = ["breast augmentation", "periareolar mastectomy (keyhole)", "double incision without grafts", "double incision with grafts", "t anchor double incision"]
-  #TOP_IDS = TOP.collect! {|e| Procedure.find_by_name(e).id}
+  TOP_IDS = TOP.collect! {|e| Procedure.find_by_name(e).id} unless Procedure.all.empty?
 
   BOTTOM = ["vaginoplasty", "phalloplasty", "metoidioplasty"]
-  #BOTTOM_IDS = BOTTOM.collect! {|e| Procedure.find_by_name(e).id}
+  BOTTOM_IDS = BOTTOM.collect! {|e| Procedure.find_by_name(e).id} unless Procedure.all.empty?
 
   PROCEDURES = Pin.uniq.pluck(:procedure_id)
   SURGEONS = Pin.uniq.pluck(:surgeon_id)

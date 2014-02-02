437da54745ae337c48d4f6499a219776c76850f3
Alex Moore-Niemi <alex@promoboxx.com>
2014-02-02 10:55:36 -0500

    only using precompiled assets locally

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -40,6 +40,7 @@ Transbucket::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  config.assets.compile = false
 
   # Expands the lines which load the assets
   config.assets.debug = true

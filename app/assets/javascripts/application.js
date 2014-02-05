cd9be04942895bb023cf5dae943e2ae14a73019f
Alex Moore-Niemi <alex@promoboxx.com>
2014-02-05 13:31:34 -0500

    beginning jasmine and backbone support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,4 +22,10 @@
 //= require jquery.multiselect
 //= require jquery.multiselect.filter
 //= require jquery_nested_form
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/transbucket
+//= require underscore
 //= require_tree .

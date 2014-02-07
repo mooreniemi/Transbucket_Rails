911fbcda99d4a297773e55731859ef0d1faf4344
Alex Moore-Niemi <alex@promoboxx.com>
2014-02-06 20:57:34 -0500

    redoing scoping process, passing build waypoint

--- spec/presenters/pin_presenter_spec.rb
+++ spec/presenters/pin_presenter_spec.rb
@@ -7,8 +7,47 @@ describe PinPresenter do
 
     pins = FactoryGirl.create_list(:pin, 3)
 
-    presenter = PinPresenter.new({user: user})
+    presenter = PinPresenter.new({current_user: user})
 
-    expect(presenter.all_pins).to eq(pins.reverse)
+    expect(presenter.all).to eq(pins.reverse)
+  end
+
+  it 'returns pins scoped by surgeon' do
+    surgeon = create(:surgeon)
+    user = create(:user)
+
+    pins = FactoryGirl.create_list(:pin, 3)
+
+    pins.last.update_attributes(surgeon_id: surgeon.id)
+
+    presenter = PinPresenter.new({current_user: user, surgeon: surgeon.id})
+
+    expect(presenter.all.last).to eq(pins.last)
+  end
+
+  it 'returns pins scoped by procedure' do
+    procedure = create(:procedure)
+    user = create(:user)
+
+    pins = FactoryGirl.create_list(:pin, 3)
+
+    pins.last.update_attributes(procedure_id: procedure.id)
+
+    presenter = PinPresenter.new({current_user: user, procedure: procedure.id})
+
+    expect(presenter.all.last).to eq(pins.last)
+  end
+
+  it 'returns pins scoped by user' do
+    current_user = create(:user)
+    user = create(:user)
+
+    pins = FactoryGirl.create_list(:pin, 3)
+
+    pins.last.update_attributes(user_id: user.id)
+
+    presenter = PinPresenter.new({current_user: user, user: user.id})
+
+    expect(presenter.all.last).to eq(pins.last)
   end
 end

73225a855bb23c579e453949fdfe5075377a30e1
Alex Moore-Niemi <alex@promoboxx.com>
2014-03-02 23:36:29 -0500

    fixing old contact form from label

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,11 +1,10 @@
 class NotificationsMailer < ActionMailer::Base
 
-  default :from => :email
   default :to => "admin@transbucket.com"
 
   def new_message(message)
     @message = message
-    mail(:subject => "[Transbucket.com Support] #{message.subject}")
+    mail(:subject => "[Transbucket.com Support] #{message.subject}", :from => message.email)
   end
 
 end

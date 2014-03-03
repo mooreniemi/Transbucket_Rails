9777ff2f9b1015d060b1de83ec9be39a4503bbe0
Alex Moore-Niemi <alex@promoboxx.com>
2014-03-02 21:02:52 -0500

    preferred pronouns helper for the pin show comment heading

--- app/helpers/pins_helper.rb
+++ app/helpers/pins_helper.rb
@@ -1,2 +1,16 @@
 module PinsHelper
+	PRONOUN_HASH = {
+		"FTM" => "he/him/his",
+		"MTF" => "she/her/hers",
+		"GenderQueer" => "they/them/theirs",
+		"None" => "they/them/theirs"
+	}
+
+	def uses_pronouns(author_gender)
+		unless PRONOUN_HASH[author_gender].nil?
+			PRONOUN_HASH[author_gender]
+		else
+			"they/them/theirs"
+		end
+	end
 end

d50e418d6452706c9e2edb4abaf6c54a3becdc6d
Alex Moore-Niemi <alex@promoboxx.com>
2014-03-25 19:37:59 -0400

    removing defunct migrations

--- db/migrate/20140325032648_change_surgeon_id_on_pin.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSurgeonIdOnPin < ActiveRecord::Migration
-  def up
-  	change_column :pins, :surgeon_id, :integer
-  end
-
-  def down
-  	change_column :pins, :surgeon_id, :string
-  end
-end

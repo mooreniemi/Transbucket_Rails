2fbd4666b0b78077aef1e1dacd44b77670a9beeb
Alex Moore-Niemi <alex@promoboxx.com>
2014-03-25 19:37:59 -0400

    removing defunct migrations

--- db/migrate/20140325032628_change_procedure_id_on_pin.rb
+++
@@ -1,9 +0,0 @@
-class ChangeProcedureIdOnPin < ActiveRecord::Migration
-  def up
-  	change_column :pins, :procedure_id, :integer
-  end
-
-  def down
-  	change_column :pins, :procedure_id, :string
-  end
-end

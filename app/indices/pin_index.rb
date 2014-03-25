354a3fb8024f52963d153b6ca0799f0ff30c1005
Alex Moore-Niemi <alex@promoboxx.com>
2014-03-24 23:11:23 -0400

    updating thinking sphinx

--- app/indices/pin_index.rb
+++ app/indices/pin_index.rb
@@ -1,6 +1,6 @@
 ThinkingSphinx::Index.define :pin, :with => :active_record do
-  indexes surgeon, :sortable => true
-  indexes procedure, :sortable => true
+  indexes surgeon_id, :sortable => true
+  indexes procedure_id, :sortable => true
   indexes username, :as => :user, :sortable => true
 
   has user_id, created_at, updated_at

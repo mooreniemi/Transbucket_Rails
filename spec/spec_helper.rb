fefb9e392d9292dddb5d4c629e3434a853aa1acc
Alex Moore-Niemi <alex@promoboxx.com>
2014-04-11 14:38:50 -0400

    adding simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl_rails'
 require 'capybara/rspec'
+require 'simplecov'
+SimpleCov.start
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

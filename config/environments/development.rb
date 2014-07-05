7fccdbced580bc857b981a26589151c24dffc6bf
Alex Moore-Niemi <alex@promoboxx.com>
2014-07-05 17:13:34 -0400

    updated gems and rails 4, yanked out some bad migrations, can get rspec to run

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Transbucket::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.eager_load = false
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
@@ -27,7 +29,7 @@ Transbucket::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
   config.assets.compress = false
@@ -54,9 +56,9 @@ Transbucket::Application.configure do
     :enable_starttls_auto => true
   }
 
-config.action_mailer.default_url_options = {
-  :host => "transbucket.com"
-}
+  config.action_mailer.default_url_options = {
+    :host => "transbucket.com"
+  }
 
   config.middleware.use Rails::Rack::LogTailer
 
--- config/initializers/block_ip.rb
+++ config/initializers/block_ip.rb
@@ -1,11 +1,11 @@
-Transbucket::Application.configure do
+# Transbucket::Application.configure do
 
-  config.middleware.insert_before(Rack::Lock, Rack::Block) do
-    # Add your rules with the rack-block syntax in here
-      ip_pattern '142.157.201.53' do
-      # expressions like '192.0.0.' also available
-        halt 404
-      end
-  end
+#   config.middleware.insert_before(Rack::Lock, Rack::Block) do
+#     # Add your rules with the rack-block syntax in here
+#       ip_pattern '142.157.201.53' do
+#       # expressions like '192.0.0.' also available
+#         halt 404
+#       end
+#   end
 
-end
+# end

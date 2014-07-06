51410ee9b5fc840cb9dc9cdf83ada7773d09a570
Alex Moore-Niemi <alex@promoboxx.com>
2014-07-06 11:51:26 -0400

    updating controller and helper specs

--- spec/helpers/sanitize_names_spec.rb
+++ spec/helpers/sanitize_names_spec.rb
@@ -6,12 +6,12 @@ describe SanitizeNames do
 
   it "can sanitize a surgeons last_name" do
     surgeon.last_name = 'bogart M.D.'
-    surgeon.sanitize_last_name.should eq('bogart')
+    expect(surgeon.sanitize_last_name).to eq('bogart')
   end
 
   it "can sanitize an incoming attribute" do
     attrs = attributes_for(:surgeon)
     attrs.extend(SanitizeNames)
-    attrs.sanitize_last_name.should be_true
+    expect(attrs.sanitize_last_name).to be_truthy
   end
 end

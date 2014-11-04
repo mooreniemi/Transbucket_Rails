7673052ceebee77d9de02a2bd8a948a92ad94cc2
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-03 23:03:09 -0500

    removing all refs to local env, dev only now, beginning to poke at performance concerns on pin presenter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable = true
     #Bullet.alert = true
-    #Bullet.bullet_logger = true
+    Bullet.bullet_logger = true
     #Bullet.console = true
     #Bullet.growl = true
     #Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20141104031258) do
     t.integer  "asset_file_size"
     t.datetime "asset_updated_at"
     t.integer  "pin_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
@@ -34,25 +34,19 @@ ActiveRecord::Schema.define(version: 20141104031258) do
     t.integer  "commentable_id",   default: 0
     t.string   "commentable_type", default: ""
     t.string   "title",            default: ""
-    t.text     "body"
+    t.text     "body",             default: ""
     t.string   "subject",          default: ""
     t.integer  "user_id",          default: 0,  null: false
     t.integer  "parent_id"
     t.integer  "lft"
     t.integer  "rgt"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "state"
   end
 
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-
-  create_table "currency", id: false, force: true do |t|
-    t.string  "currency", limit: 3,                                      null: false
-    t.decimal "rate",                precision: 3, scale: 0, default: 0, null: false
-    t.string  "symbol",   limit: 10,                                     null: false
-  end
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
@@ -64,63 +58,28 @@ ActiveRecord::Schema.define(version: 20141104031258) do
     t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "genders", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "messages", force: true do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "old_surgeons", force: true do |t|
-    t.string "SurgeonName", limit: 40,                null: false
-    t.string "Address",     limit: 400
-    t.string "City",        limit: 40
-    t.string "State",       limit: 20
-    t.string "ZIP",         limit: 5
-    t.string "Country",     limit: 40,                null: false
-    t.string "Phone",       limit: 20
-    t.string "Email",       limit: 40
-    t.string "URL",         limit: 80
-    t.string "Procedures",  limit: 400
-    t.string "FTM-Bottom",  limit: 1,   default: "0", null: false
-    t.string "FTM-Top",     limit: 1,   default: "0", null: false
-    t.string "MTF-Bottom",  limit: 1,   default: "0", null: false
-    t.string "MTF-Top",     limit: 1,   default: "0", null: false
-    t.string "Facial",      limit: 1,   default: "0", null: false
-    t.string "HairRemoval", limit: 1,   default: "0", null: false
-    t.string "Notes",       limit: 800
-  end
-
-  create_table "old_users", primary_key: "ID", force: true do |t|
-    t.string   "username",   limit: 60
-    t.string   "password",   limit: 120
-    t.string   "name",       limit: 40,                        null: false
-    t.string   "sex",        limit: 40,  default: "undefined", null: false
-    t.string   "email",      limit: 80,                        null: false
-    t.string   "Activation", limit: 40
-    t.string   "contact",    limit: 4,   default: "no",        null: false
-    t.string   "anonymous",  limit: 4,   default: "no",        null: false
-    t.integer  "numImages",              default: 0,           null: false
-    t.datetime "dateJoined",                                   null: false
-    t.datetime "lastLogin",                                    null: false
-    t.string   "currency",   limit: 3,   default: "USD",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "pin_images", force: true do |t|
-    t.string   "caption",            limit: 555
+    t.string   "caption"
     t.integer  "pin_id"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
@@ -131,8 +90,8 @@ ActiveRecord::Schema.define(version: 20141104031258) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
@@ -153,47 +112,20 @@ ActiveRecord::Schema.define(version: 20141104031258) do
 
   create_table "preferences", force: true do |t|
     t.integer  "user_id"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "notification", default: true
     t.boolean  "safe_mode",    default: false
   end
 
   create_table "procedures", force: true do |t|
     t.string   "name"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "body_type"
     t.string   "gender"
     t.integer  "avg_sensation"
     t.integer  "avg_satisfaction"
-  end
-
-  create_table "results", primary_key: "ID", force: true do |t|
-    t.string   "username",     limit: 40,                     null: false
-    t.date     "surgeryDate"
-    t.string   "surgeon",      limit: 40,   default: "other", null: false
-    t.string   "surgeryType",  limit: 40,                     null: false
-    t.integer  "cost"
-    t.string   "comments",     limit: 1000
-    t.string   "wantRevision", limit: 20
-    t.string   "img1",         limit: 40
-    t.string   "img2",         limit: 40
-    t.string   "img3",         limit: 40
-    t.string   "img4",         limit: 40
-    t.date     "img1date"
-    t.date     "img2date"
-    t.date     "img3date"
-    t.date     "img4date"
-    t.string   "img1com",      limit: 400
-    t.string   "img2com",      limit: 400
-    t.string   "img3com",      limit: 400
-    t.string   "img4com",      limit: 400
-    t.string   "anonymous",    limit: 4,    default: "no",    null: false
-    t.string   "insurance",    limit: 4,    default: "no",    null: false
-    t.string   "moderated",    limit: 4,    default: "0",     null: false
-    t.datetime "dateApproved",                                null: false
-    t.string   "currencyCode", limit: 3,    default: "USD",   null: false
   end
 
   create_table "settings", force: true do |t|
@@ -201,8 +133,8 @@ ActiveRecord::Schema.define(version: 20141104031258) do
     t.text     "value"
     t.integer  "target_id",   null: false
     t.string   "target_type", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "settings", ["target_type", "target_id", "var"], name: "index_settings_on_target_type_and_target_id_and_var", unique: true, using: :btree
@@ -210,13 +142,12 @@ ActiveRecord::Schema.define(version: 20141104031258) do
   create_table "skills", force: true do |t|
     t.integer  "surgeon_id"
     t.integer  "procedure_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "surgeons", force: true do |t|
     t.string   "first_name"
-    t.string   "last_name"
     t.string   "address"
     t.string   "city"
     t.string   "state"
@@ -226,8 +157,9 @@ ActiveRecord::Schema.define(version: 20141104031258) do
     t.string   "email"
     t.string   "url"
     t.text     "procedure_list"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "last_name"
     t.text     "notes"
   end
 
@@ -251,20 +183,6 @@ ActiveRecord::Schema.define(version: 20141104031258) do
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
-  create_table "therapists", id: false, force: true do |t|
-    t.string "id",            limit: 4,                   null: false
-    t.string "therapistName", limit: 40,                  null: false
-    t.string "affiliation",   limit: 100
-    t.string "address",       limit: 80
-    t.string "city",          limit: 80,                  null: false
-    t.string "state",         limit: 2,                   null: false
-    t.string "Country",       limit: 16,  default: "USA", null: false
-    t.string "phone",         limit: 40
-    t.string "email",         limit: 40
-    t.string "website",       limit: 80
-    t.string "notes",         limit: 800
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -276,8 +194,8 @@ ActiveRecord::Schema.define(version: 20141104031258) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
     t.string   "gender_id"
     t.string   "username"

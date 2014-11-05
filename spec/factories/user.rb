cbdd0507ed78a15e67a71eb8383272f8003ca6d0
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-04 20:15:35 -0500

    tiny bit of refactoring, transactional fixtures to prevent faker brittleness

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,8 @@
 
 FactoryGirl.define do
   factory :user do
-    username { Faker::Internet.user_name}
-    name { Faker::Name.name}
+    username {Faker::Internet.user_name}
+    name {Faker::Name.name}
     email {Faker::Internet.free_email}
     password {Faker::Internet.password}
   end

3879fe180fe6b3ebe3ac071b8dc70b5db79d0b88
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-05 21:47:00 -0500

    back to green rspec, pin presenter first pass

--- app/presenters/pin_presenter.rb
+++ app/presenters/pin_presenter.rb
@@ -1,36 +1,56 @@
 class PinPresenter
-  using SafeFetch
-  attr_accessor :query, :user, :query, :procedures, :surgeons, :page, :general
+  attr_accessor :query, :page, :filter, :pins
+  attr_accessor :user, :procedures, :surgeons, :general
 
   def initialize(opts = {})
-    @page = opts.safe_fetch(:page)
+    Pin.includes(:comments) # TODO only improves from 1500 ms to 1200 :c
 
-    @query = opts.safe_fetch(:query) { [] }
+    @page = opts.delete(:page)
+    @query = opts.delete(:query)
+    @user = opts.delete(:user)
+    @filter = opts
 
-    @user = opts.safe_fetch(:user)
-    @procedures = [opts.safe_fetch(:procedure)]
-    @surgeons = [opts.safe_fetch(:surgeon)]
-
-    @general = format_scope(opts.safe_fetch(:scope)) if opts[:scope].present?
+    @pins = if @query.present?
+      Pin.search(@query, :page => @page, :per_page => 20)
+    elsif @user.present?
+      Pin.by_user(@user).paginate(:page => @page)
+    else
+      Pin.recent.paginate(:page => @page)
+    end
   end
 
   def all
-    Pin.includes(:comments) # TODO only improves from 1500 ms to 1200 :c
-    pins = query.blank? ? Pin.recent.paginate(:page => page) : Pin.search(query, :page => page, :per_page => 20)
-
-    pins = Pin.by_user(user).paginate(:page => page) if user.present?
-    general.each {|s| pins = pins.send(s)} if general.present?
-
-    procedures.each {|p| pins = pins.by_procedure(p)} if procedures.present?
-    surgeons.each {|s| pins = pins.by_surgeon(s)} if surgeons.present?
-
+    build_query if has_keywords?
     pins.uniq!
-    pins
   end
 
   private
+  def build_query
+    set_keywords
+
+    if surgeons.present?
+      [surgeons].each {|s| @pins = @pins.by_surgeon(s)}
+    elsif procedures.present?
+      [procedures].each {|p| @pins = @pins.by_procedure(p)}
+    else
+      general.each {|s| @pins = @pins.send(s)}
+    end
+  end
+
   def format_scope(scope)
+    return if scope.nil?
     scope.collect!(&:parameterize).collect!(&:underscore).collect!(&:to_sym)
     scope
+  end
+
+  def has_keywords?
+    filter.values.reject(&:nil?).count > 0
+  end
+
+  def set_keywords
+    @user = filter[:user]
+    @procedures = filter[:procedure]
+    @surgeons = filter[:surgeon]
+    @general = format_scope(filter.fetch(:scope,nil))
   end
 end

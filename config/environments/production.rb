54010dc2a4f0cd0bc2abc327759d79f22ebc059d
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-08 16:16:02 -0500

    adding default host to action_mailer for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "www.transbucket.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -79,6 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 
   # see https://devcenter.heroku.com/articles/sendgrid#ruby-rails
   ActionMailer::Base.smtp_settings = {

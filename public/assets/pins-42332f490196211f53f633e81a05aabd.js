2386e75a0a769c5dedf8994e271d4b384bfbf7ed
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-08 17:27:15 -0500

    precompiling assets

---
+++ public/assets/pins-42332f490196211f53f633e81a05aabd.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#scope").multiselect({position:{my:"bottom",at:"center"},noneSelectedText:"General Options"}),$("#surgeon").multiselect({position:{my:"bottom",at:"center"},noneSelectedText:"Select Surgeon(s)"}),$("#procedure").multiselect({position:{my:"bottom",at:"center"},noneSelectedText:"Select Procedure(s)"})});

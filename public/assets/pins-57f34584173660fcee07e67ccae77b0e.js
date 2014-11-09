62cebe4b5f42e395396fb72949e4dcfb8c2fd154
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-09 17:11:42 -0500

    switching multiselect libraries, as jquery-multiselect hasnt been updated since 2012

---
+++ public/assets/pins-57f34584173660fcee07e67ccae77b0e.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#scope").multiSelect({afterSelect:function(){$("#submit-filter").trigger("click")}}),$("#surgeon").multiSelect({afterSelect:function(){$("#submit-filter").trigger("click")}}),$("#procedure").multiSelect({afterSelect:function(){$("#submit-filter").trigger("click")}}),$(".label-with-popover").popover()});

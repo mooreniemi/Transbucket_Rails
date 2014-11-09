e32d8b2dbbe9439f834a33c38b3f1e88eafafb16
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-09 15:09:14 -0500

    removing tinymce from bower asset management, as using tinymce gem seems better

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module Transbucket
     # necessary for using bower-rails!
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'jquery-ui', 'themes', 'smoothness', 'images')
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'tinymce', 'skins', 'lightgray', 'imag')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'tinymce', 'skins', 'lightgray', 'img')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components', 'tinymce', 'skins', 'lightgray', 'fonts')
 
     config.generators do |g|
       g.test_framework :rspec,

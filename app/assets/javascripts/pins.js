2fc27d34d71f4fdd41538539a4323b967515975e
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-09 23:47:14 -0500

    continuing work on filter ui

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,24 +1,18 @@
 $(document).ready(function() {
+    $("#clear-filter").click(function() {
+        $("#filter_dropdown").multiSelect('deselect_all')
+    });
     $("#scope").multiSelect({
-        afterSelect: function() {
-            $("#submit-filter").trigger("click");
-        },
         selectableHeader: "General categories",
         selectionHeader: "Selected general"
     });
 
     $("#surgeon").multiSelect({
-        afterSelect: function() {
-            $("#submit-filter").trigger("click");
-        },
         selectableHeader: "Surgeons",
         selectionHeader: "Selected surgeons"
     });
 
     $("#procedure").multiSelect({
-        afterSelect: function() {
-            $("#submit-filter").trigger("click");
-        },
         selectableHeader: "Procedures",
         selectionHeader: "Selected procedures"
     });

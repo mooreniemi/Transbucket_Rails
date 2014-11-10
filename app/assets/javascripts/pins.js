9355186737152813c0d70a245fec79113ad019dd
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-09 20:35:45 -0500

    breaking out more of header into partials

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -2,19 +2,25 @@ $(document).ready(function() {
     $("#scope").multiSelect({
         afterSelect: function() {
             $("#submit-filter").trigger("click");
-        }
+        },
+        selectableHeader: "General categories",
+        selectionHeader: "Selected general"
     });
 
     $("#surgeon").multiSelect({
         afterSelect: function() {
             $("#submit-filter").trigger("click");
-        }
+        },
+        selectableHeader: "Surgeons",
+        selectionHeader: "Selected surgeons"
     });
 
     $("#procedure").multiSelect({
         afterSelect: function() {
             $("#submit-filter").trigger("click");
-        }
+        },
+        selectableHeader: "Procedures",
+        selectionHeader: "Selected procedures"
     });
 
     $('.label-with-popover').popover();

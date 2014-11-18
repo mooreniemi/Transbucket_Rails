b137a4efb4d1c904a8db13db4c7a9ce863a79798
Alex Moore-Niemi <amooreniemi@zipcar.com>
2014-11-18 09:32:12 -0500

    adding cat.jpg fixture

--- spec/factories/pin_image.rb
+++ spec/factories/pin_image.rb
@@ -1,9 +1,6 @@
 FactoryGirl.define do
-	factory :pin_image do
-		#association :pin
-		#photo fixture_file_upload('/public/system/pin_images/cat.jpg', 'image/jpg')
-    photo {File.new(Rails.root.join("public", "system", "pin_images", "cat.jpg"))}
+  factory :pin_image do
+    photo {File.new(Rails.root.join("spec", "fixtures", "cat.jpg"))}
     caption 'this is a caption'
-    #pin_id
-	end
+  end
 end

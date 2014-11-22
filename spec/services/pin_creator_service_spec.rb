a0cdbe63bdcde044606b23b2d105310cef5df0ad
Alex Moore-Niemi <alex@promoboxx.com>
2014-11-22 16:54:51 -0500

    wip, rearranging the submit form page

--- spec/services/pin_creator_service_spec.rb
+++ spec/services/pin_creator_service_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe PinCreatorService, '#create' do
   let!(:user) { create(:user) }
-  let(:pin_params) { {"pin" => attributes_for(:pin)}}
+  let(:pin_params) { attributes_for(:pin).merge!({pin_image_ids: "1"})}
   let(:surgeon_params) { attributes_for(:surgeon) }
   let(:procedure_params) { attributes_for(:procedure) }
 

2ea633a485b939390b28bd51e1354ef53e01f09c
Alex Moore-Niemi <alex@promoboxx.com>
2015-04-25 17:08:38 -0400

    updating ruby version to 2.2.2 and updating gems

--- spec/services/pin_updater_service_spec.rb
+++ spec/services/pin_updater_service_spec.rb
@@ -4,15 +4,13 @@ describe PinUpdaterService, '#santize_last_name' do
   it "can sanitize a surgeon's name" do
     attrs = attributes_for(:surgeon)
     attrs[:last_name] = "dippy-dot M.D."
-    pin = create(:pin)
-    user = create(:user)
+    pin, user = create(:pin), create(:user)
     params = {
       pin_id: pin.id,
       surgeon_attributes: attrs,
       procedure_attributes: attributes_for(:procedure)
     }
-    service = PinUpdaterService.new(params,user)
-    service.update
+    PinUpdaterService.new(params,user).update
     expect(pin.reload.surgeon.last_name).to eq('dippy-dot')
   end
 end

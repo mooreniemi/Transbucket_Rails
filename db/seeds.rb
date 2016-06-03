c75a73066147210869da07cb0aa0b4e604b77198
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-03 18:59:45 -0400

    changing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
-require 'factory_girl_rails'
 require 'faker'
 
-10.times do |i|
-  FactoryGirl.create :pin
+25.times do |i|
+  Pin.create(
+    id: Faker::Number.number(5),
+    description: Faker::Hacker.say_something_smart,
+    surgeon_id: Random.rand(11),
+    procedure_id: Random.rand(10),
+    user_id: Faker::Number(5),
+    cost: Random.rand(50000),
+    sensation: Random.rand(11),
+    satisfaction: Random.rand(11),
+    pin_images: PinImage.create(
+      photo: "",
+      caption: Faker::Hacker.say_something_smart
+    )
+  )
   puts "created ##{i} pin"
 end
+
 puts "created 10 pins"

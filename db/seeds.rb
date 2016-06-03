fa3f4be3b3e43014d248e54865210039ecf9d51e
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-03 19:02:58 -0400

    seed had some typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,16 @@ require 'faker'
     description: Faker::Hacker.say_something_smart,
     surgeon_id: Random.rand(11),
     procedure_id: Random.rand(10),
-    user_id: Faker::Number(5),
+    user_id: Faker::Number.number(5),
     cost: Random.rand(50000),
     sensation: Random.rand(11),
     satisfaction: Random.rand(11),
-    pin_images: PinImage.create(
-      photo: "",
-      caption: Faker::Hacker.say_something_smart
-    )
+    pin_images: [
+      PinImage.create(
+        photo: "",
+        caption: Faker::Hacker.say_something_smart
+      )
+    ]
   )
   puts "created ##{i} pin"
 end

4133088fdd459e808cd0487303cbbfd163156a78
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-03 12:01:52 -0400

    updating readme a bit, bumping gems

--- db/schema.rb
+++ db/schema.rb
@@ -16,85 +16,85 @@ ActiveRecord::Schema.define(version: 20150425182913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "assets", force: true do |t|
-    t.string   "asset_file_name"
-    t.string   "asset_content_type"
+  create_table "assets", force: :cascade do |t|
+    t.string   "asset_file_name",    limit: 255
+    t.string   "asset_content_type", limit: 255
     t.integer  "asset_file_size"
     t.datetime "asset_updated_at"
     t.integer  "pin_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
 
-  create_table "comments", force: true do |t|
-    t.integer  "commentable_id",   default: 0
-    t.string   "commentable_type", default: ""
-    t.string   "title",            default: ""
-    t.text     "body",             default: ""
-    t.string   "subject",          default: ""
-    t.integer  "user_id",          default: 0,  null: false
+  create_table "comments", force: :cascade do |t|
+    t.integer  "commentable_id",               default: 0
+    t.string   "commentable_type", limit: 255, default: ""
+    t.string   "title",            limit: 255, default: ""
+    t.text     "body",                         default: ""
+    t.string   "subject",          limit: 255, default: ""
+    t.integer  "user_id",                      default: 0,  null: false
     t.integer  "parent_id"
     t.integer  "lft"
     t.integer  "rgt"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "state"
+    t.string   "state",            limit: 255
   end
 
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
-  create_table "delayed_jobs", force: true do |t|
-    t.integer  "priority",   default: 0, null: false
-    t.integer  "attempts",   default: 0, null: false
-    t.text     "handler",                null: false
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",               default: 0, null: false
+    t.integer  "attempts",               default: 0, null: false
+    t.text     "handler",                            null: false
     t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
+    t.string   "locked_by",  limit: 255
+    t.string   "queue",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
-  create_table "genders", force: true do |t|
-    t.string   "name"
+  create_table "genders", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "messages", force: true do |t|
+  create_table "messages", force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "pin_images", force: true do |t|
-    t.string   "caption"
+  create_table "pin_images", force: :cascade do |t|
+    t.string   "caption",            limit: 255
     t.integer  "pin_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
+    t.string   "photo_file_name",    limit: 255
+    t.string   "photo_content_type", limit: 255
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
   end
 
   add_index "pin_images", ["pin_id"], name: "index_pin_images_on_pin_id", using: :btree
 
-  create_table "pins", force: true do |t|
-    t.string   "description"
+  create_table "pins", force: :cascade do |t|
+    t.string   "description",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "procedure_id"
@@ -102,15 +102,15 @@ ActiveRecord::Schema.define(version: 20150425182913) do
     t.integer  "surgeon_id"
     t.text     "details"
     t.integer  "cost"
-    t.string   "username"
-    t.string   "state"
+    t.string   "username",           limit: 255
+    t.string   "state",              limit: 255
     t.integer  "sensation"
     t.integer  "satisfaction"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
 
-  create_table "preferences", force: true do |t|
+  create_table "preferences", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -118,57 +118,57 @@ ActiveRecord::Schema.define(version: 20150425182913) do
     t.boolean  "safe_mode",    default: false
   end
 
-  create_table "procedures", force: true do |t|
-    t.string   "name"
+  create_table "procedures", force: :cascade do |t|
+    t.string   "name",             limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "body_type"
-    t.string   "gender"
+    t.string   "body_type",        limit: 255
+    t.string   "gender",           limit: 255
     t.integer  "avg_sensation"
     t.integer  "avg_satisfaction"
   end
 
-  create_table "settings", force: true do |t|
-    t.string   "var",         null: false
+  create_table "settings", force: :cascade do |t|
+    t.string   "var",         limit: 255, null: false
     t.text     "value"
-    t.integer  "target_id",   null: false
-    t.string   "target_type", null: false
+    t.integer  "target_id",               null: false
+    t.string   "target_type", limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "settings", ["target_type", "target_id", "var"], name: "index_settings_on_target_type_and_target_id_and_var", unique: true, using: :btree
 
-  create_table "skills", force: true do |t|
+  create_table "skills", force: :cascade do |t|
     t.integer  "surgeon_id"
     t.integer  "procedure_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "surgeons", force: true do |t|
-    t.string   "first_name"
-    t.string   "address"
-    t.string   "city"
-    t.string   "state"
+  create_table "surgeons", force: :cascade do |t|
+    t.string   "first_name",     limit: 255
+    t.string   "address",        limit: 255
+    t.string   "city",           limit: 255
+    t.string   "state",          limit: 255
     t.integer  "zip"
-    t.string   "country"
+    t.string   "country",        limit: 255
     t.integer  "phone"
-    t.string   "email"
-    t.string   "url"
+    t.string   "email",          limit: 255
+    t.string   "url",            limit: 255
     t.text     "procedure_list"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "last_name"
+    t.string   "last_name",      limit: 255
     t.text     "notes"
   end
 
-  create_table "taggings", force: true do |t|
+  create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
-    t.string   "taggable_type"
+    t.string   "taggable_type", limit: 255
     t.integer  "tagger_id"
-    t.string   "tagger_type"
+    t.string   "tagger_type",   limit: 255
     t.string   "context",       limit: 128
     t.datetime "created_at"
   end
@@ -176,48 +176,48 @@ ActiveRecord::Schema.define(version: 20150425182913) do
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
-  create_table "tags", force: true do |t|
-    t.string  "name"
-    t.integer "taggings_count", default: 0
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",           limit: 255
+    t.integer "taggings_count",             default: 0
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "gender_id"
-    t.string   "username"
-    t.boolean  "admin",                  default: false
-    t.string   "md5"
-    t.string   "confirmation_token"
+    t.string   "name",                   limit: 255
+    t.string   "gender_id",              limit: 255
+    t.string   "username",               limit: 255
+    t.boolean  "admin",                              default: false
+    t.string   "md5",                    limit: 255
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
+    t.string   "unconfirmed_email",      limit: 255
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "votes", force: true do |t|
+  create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
-    t.string   "votable_type"
+    t.string   "votable_type", limit: 255
     t.integer  "voter_id"
-    t.string   "voter_type"
+    t.string   "voter_type",   limit: 255
     t.boolean  "vote_flag"
-    t.string   "vote_scope"
+    t.string   "vote_scope",   limit: 255
     t.integer  "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"

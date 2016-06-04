102f3a14b111efb8ae830ece8c53a7dba23136b8
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-03 21:48:34 -0400

    updating tests for comment mailer

--- app/services/comment_service.rb
+++ app/services/comment_service.rb
@@ -1,46 +1,48 @@
 #app/services/comments_service.rb
 class CommentService
-  attr_reader :body, :commentable, :user
-  attr_accessor :comment
+	attr_reader :body, :commentable, :user
+	attr_accessor :comment
 
-  def initialize(commentable, user, body)
-    @commentable = commentable
-    @body = body
-    @user = user
-  end
+	def initialize(commentable, user, body)
+		@commentable = commentable
+		@body = body
+		@user = user
+	end
 
-  def create
-    policy = UserPolicy.new(user)
+	def create
+		policy = UserPolicy.new(user)
 
-    if policy.wants_email?
-      @comment = create_and_notify
-    else
-      @comment = build
-    end
+		if policy.wants_email?
+			@comment = create_and_notify
+		else
+			@comment = build
+		end
 
-    @comment.save
-  end
+		@comment.save
+	end
 
-  private
+	private
 
-  def build
-    Comment.build_from(commentable, user, body)
-  end
+	def build
+		Comment.build_from(commentable, user, body)
+	end
 
-  def create_and_notify
-    comment = build
+	# TODO we shouldnt be sending notification until we are
+	# sure the save happened without error
+	def create_and_notify
+		comment = build
 
-    begin
-      send_email_notification(comment)
-    rescue => e
-      puts "#{e} was raised while attempting to send notification " +
-        "on #{commentable.class} #{commentable.id} to User #{user.id}"
-    end
+		begin
+			send_email_notification(comment)
+		rescue => e
+			puts "#{e} was raised while attempting to send notification " +
+				"on #{commentable.class} #{commentable.id} to User #{user.id}"
+		end
 
-    comment
-  end
+		comment
+	end
 
-  def send_email_notification(comment)
-    CommentMailer.new_comment_email(user.id, commentable.id).deliver_now
-  end
+	def send_email_notification(comment)
+		CommentMailer.new_comment_email(user.id, commentable.id).deliver_now
+	end
 end
--- spec/mailers/comment_mailer_spec.rb
+++ spec/mailers/comment_mailer_spec.rb
@@ -1,15 +1,18 @@
 require "spec_helper"
 
 describe CommentMailer do
-  let(:user) { build(:user) }
-  let(:commentable) { build(:pin, user: user) }
-  let(:body) {"all right"}
-  let(:comments_service) {CommentService.new(commentable, user, body)}
-  let(:mail) {CommentMailer.new_comment_email(comments_service)}
-  it "should have the right subject line" do
-    expect(mail.subject).to eq("New Comments on Your Submission")
-  end
-  it "should have the right url" do
-    expect(mail.body.raw_source).to include("http://www.transbucket.com/pins/#{commentable.id}?utm_source=comment_reminder&amp;utm_medium=email&amp;utm_campaign=comment_reminder")
-  end
+	let(:user) { create(:user) }
+	let(:commentable) { build(:pin, user: user) }
+	let(:body) {"all right"}
+	let(:mail) do
+		CommentMailer.new_comment_email(user.id, commentable.id)
+	end
+	it "should have the right subject line" do
+		expect(mail.subject).to eq("Transbucket.com: New Comments on #{commentable.id}")
+	end
+	it "should have the right url" do
+		comment_url = "http://www.transbucket.com/pins/#{commentable.id}" +
+			"?utm_source=comment_reminder&amp;utm_medium=email&amp;utm_campaign=comment_reminder"
+		expect(mail.body.raw_source).to include(comment_url)
+	end
 end

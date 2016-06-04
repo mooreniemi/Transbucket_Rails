b01d6a7c38e5a7041386d93c48a74eeb601fa2f9
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-03 23:34:23 -0400

    making sure comments cant get too long

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
-	factory :comment do
-		association :user
-		association :commentable, factory: :pin
-		body Faker::Lorem.paragraph
-    	title Faker::Lorem.sentence
-	end
+  factory :comment do
+    association :user
+    association :commentable, factory: :pin
+    body Faker::Lorem.paragraph
+    title Faker::Lorem.sentence
+  end
+
+  trait :big_and_unspaced do
+    body Faker::Lorem.paragraph.delete(' ')
+  end
 end

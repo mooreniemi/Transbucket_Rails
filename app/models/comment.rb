b05b2d9a3b77ddd5b8f268cc013d97989b8b463b
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-03 23:34:23 -0400

    making sure comments cant get too long

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,58 +1,62 @@
 # Comments are applied on submissions, no threading on each other currently.
 # Much of this class was generated from commentable gem.
 class Comment < ActiveRecord::Base
-  include AASM
-  validates :body, presence: true
-  validates :user, presence: true
+	include AASM
+	validates :body, presence: true
+	validates :user, presence: true
 
-  acts_as_votable
+	acts_as_votable
 
-  belongs_to :commentable, polymorphic: true
-  belongs_to :user
+	belongs_to :commentable, polymorphic: true
+	belongs_to :user
 
-  aasm column: :state do
-    state :pending, value: 'pending'
-    state :published, value: 'published', initial: :published
+	aasm column: :state do
+		state :pending, value: 'pending'
+		state :published, value: 'published', initial: :published
 
-    event :publish do
-      transitions from: :pending, to: :published
-    end
+		event :publish do
+			transitions from: :pending, to: :published
+		end
 
-    event :review do
-      transitions from: :published, to: :pending
-    end
-  end
+		event :review do
+			transitions from: :published, to: :pending
+		end
+	end
 
-  def flags
-    votes
-  end
+	def flags
+		votes
+	end
 
-  def snippet
-    body.split(' ').first(5).join(' ')
-  end
+	def snippet
+		if body.include?(' ')
+			body.split(' ').first(5).join(' ')
+		else
+			body[0..49]
+		end
+	end
 
-  def self.find_comments_by_user(user)
-    where(user_id: user.id).order('created_at DESC')
-  end
+	def self.find_comments_by_user(user)
+		where(user_id: user.id).order('created_at DESC')
+	end
 
-  def self.find_comments_for_commentable(commentable_str, commentable_id)
-    where(commentable_type: commentable_str.to_s,
-          commentable_id: commentable_id).
-          order('created_at DESC')
-  end
+	def self.find_comments_for_commentable(commentable_str, commentable_id)
+		where(commentable_type: commentable_str.to_s,
+					commentable_id: commentable_id).
+					order('created_at DESC')
+	end
 
-  def self.new_as_of(last_login_time)
-    where("created_at > ? and state = 'published'", last_login_time)
-  end
+	def self.new_as_of(last_login_time)
+		where("created_at > ? and state = 'published'", last_login_time)
+	end
 
-  def self.find_commentable(commentable_str, commentable_id)
-    commentable_str.constantize.find(commentable_id)
-  end
+	def self.find_commentable(commentable_str, commentable_id)
+		commentable_str.constantize.find(commentable_id)
+	end
 
-  def self.build_from(obj, user_id, comment)
-    new \
-      commentable: obj,
-      body: comment,
-      user_id: user_id.id
-  end
+	def self.build_from(obj, user_id, comment)
+		new \
+			commentable: obj,
+			body: comment,
+			user_id: user_id.id
+	end
 end

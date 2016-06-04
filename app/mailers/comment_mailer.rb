06409739883b3c1b6da2f3d09310bb20bf4060de
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-03 21:48:34 -0400

    updating tests for comment mailer

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -3,7 +3,7 @@ class CommentMailer < ActionMailer::Base
 
   def new_comment_email(receiver_id, about_id)
     @user = User.find(receiver_id)
-    @url  = 'http://www.transbucket.com/pins/' + about_id
-    mail(to: @user.email, subject: 'New Comments on Your Submission')
+    @url  = 'http://www.transbucket.com/pins/' + about_id.to_s
+    mail(to: @user.email, subject: "Transbucket.com: New Comments on #{about_id}")
   end
 end

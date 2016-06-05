e3e7d1cc93aeeb39f286f33a27cbe9f07de1f3bc
Cora Johnson-Roberson <me@corajr.com>
2016-06-05 13:21:15 -0400

    bower rails resolve static asset urls

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -3,8 +3,8 @@ BowerRails.configure do |bower_rails|
   bower_rails.install_before_precompile = true
 
   # Invoke `rake bower:resolve` automatically before the `rake assets:precompile` task.
-  # bower_rails.resolve_before_precompile = true
+  bower_rails.resolve_before_precompile = true
 
   # Invoke `rake bower:clean` automatically before the `rake assets:precompile` task.
-  # bower_rails.clean_before_precompile = true  
+  # bower_rails.clean_before_precompile = true
 end

0014d40469db5c164c1aef1162540bfce8f2565d
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-04 23:54:36 -0400

    fixing procedure and surgeon forms

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -8,12 +8,17 @@ class ProceduresController < ApplicationController
   end
 
   def create
-    @procedure = Procedure.new(params[:procedure])
+    @procedure = Procedure.new(procedure_params)
 
     if @procedure.save
       respond_to do |format|
         format.js  { @procedures = Procedure.order('name ASC'); @procedure }
       end
     end
+  end
+
+  private
+  def procedure_params
+    params.require(:procedure).permit(:name, :body_type, :gender)
   end
 end

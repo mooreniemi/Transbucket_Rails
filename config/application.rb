f74398736e49ae16218b60b330dadf54a5f73a77
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-05 13:28:39 -0400

    more caching

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Transbucket
   class Application < Rails::Application
+    config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/page_cache"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

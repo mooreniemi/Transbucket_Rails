3fa3c3f43cc62a1326ab8e2d25aa546bf699eca1
Cora Johnson-Roberson <me@corajr.com>
2016-06-05 13:36:24 -0400

    include images and fonts in asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,4 @@
 Rails.application.config.assets.precompile += %w( search.js pins.js pins/index.js pins/edit.js pins/new.js rated_divs.js )
+
+types = %w( *.png *.gif *.jpg *.eot *.woff *.woff2 *.ttf)
+Rails.application.config.assets.precompile += types

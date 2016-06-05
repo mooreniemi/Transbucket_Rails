f1987417e5f95a6960b57a51c14edf55ba7a8da9
Cora Johnson-Roberson <me@corajr.com>
2016-06-05 17:46:04 -0400

    change to mock image for pin image factory

--- spec/factories/pin_image.rb
+++ spec/factories/pin_image.rb
@@ -1,7 +1,9 @@
 include ActionDispatch::TestProcess
 FactoryGirl.define do
   factory :pin_image do
-    photo { fixture_file_upload(Rails.root.join("spec", "fixtures", "cat.jpg"), 'image/jpg') }
+    photo_file_name { 'cat.jpg' }
+    photo_content_type { 'image/jpg' }
+    photo_file_size { 1024 }
     caption {Faker::Lorem.sentence(3)}
   end
 end

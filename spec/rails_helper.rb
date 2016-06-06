09275e1be58095a7a75de49be26d695e68d1c249
Cora Johnson-Roberson <me@corajr.com>
2016-06-06 18:46:59 -0400

    more deterministic pin-creation feature test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -46,8 +46,8 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  config.filter_gems_from_backtrace "rack", "rake", "actionpack", "actionview", "activesupport",
-                                    "activerecord", "railties", "warden", "bullet"
+  # config.filter_gems_from_backtrace "rack", "rake", "actionpack", "actionview", "activesupport",
+  #                                   "activerecord", "railties", "warden", "bullet"
 
   config.include Devise::TestHelpers, :type => :controller
   config.include ControllerHelpers, :type => :controller

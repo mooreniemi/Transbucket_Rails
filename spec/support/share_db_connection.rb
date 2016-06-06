a32865aa52c5af6fbcba7d0f23cb587b7e4f6aed
Cora Johnson-Roberson <me@corajr.com>
2016-06-06 16:57:42 -0400

    improved db sharing in tests
    https://gist.github.com/mperham/3049152#gistcomment-1570591

--- spec/support/share_db_connection.rb
+++ spec/support/share_db_connection.rb
@@ -3,10 +3,20 @@ class ActiveRecord::Base
   @@shared_connection = nil
 
   def self.connection
-    @@shared_connection || retrieve_connection
+    @@shared_connection || ConnectionPool::Wrapper.new(:size => 1) { retrieve_connection }
   end
 end
 
-# Forces all threads to share the same connection. This works on
-# Capybara because it starts the web server in a thread.
 ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+
+# hack a mutex in the query execution so that we don't
+# get competing queries that can timeout and not get cleaned up
+module MutexLockedQuerying
+  @@semaphore = Mutex.new
+
+  def async_exec(*)
+    @@semaphore.synchronize { super }
+  end
+end
+
+PG::Connection.prepend(MutexLockedQuerying)

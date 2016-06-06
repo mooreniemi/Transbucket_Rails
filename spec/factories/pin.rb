f6ea08a0e862bcfd79f63f752e6cecef2e9d64d9
Cora Johnson-Roberson <me@corajr.com>
2016-06-06 19:51:27 -0400

    only create surgeon when necessary

--- spec/factories/pin.rb
+++ spec/factories/pin.rb
@@ -3,8 +3,8 @@ require 'faker'
 FactoryGirl.define do
   factory :pin do
     description { Faker::Lorem.sentences(3).join(" ") }
-    surgeon
-    procedure
+    surgeon_id  { Faker::Number.number(5) }
+    procedure_id  { Faker::Number.number(5) }
     user_id  { Faker::Number.number(5) }
     cost { Random.rand(50000) }
     sensation { Random.rand(11) }
@@ -15,9 +15,9 @@ FactoryGirl.define do
       comments { create_list(:comment, 2) }
     end
 
-    trait :with_surgeon_and_procedure_ids do
-      surgeon_id { Faker::Number.number(5) }
-      procedure_id { Faker::Number.number(5) }
+    trait :with_surgeon_and_procedure do
+      surgeon
+      procedure
     end
 
     trait :invalid do

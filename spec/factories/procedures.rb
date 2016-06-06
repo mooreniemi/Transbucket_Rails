f982ad2f149d66093735c4897303d423bb77a737
Cora Johnson-Roberson <me@corajr.com>
2016-06-06 15:49:27 -0400

    resolve intermittent testing failures

--- spec/factories/procedures.rb
+++ spec/factories/procedures.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :procedure do
-    id { Faker::Number.number(3) }
     name { Faker::Lorem.words(2).join(" ") }
     gender { Faker::Lorem.word }
     body_type { Faker::Lorem.word }

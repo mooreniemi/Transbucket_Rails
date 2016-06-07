c867fe822b195ffe3ef68c1d3e6f6d43f43145b5
Cora Johnson-Roberson <me@corajr.com>
2016-06-07 12:41:53 -0400

    nested forms

--- app/forms/pin_form.rb
+++ app/forms/pin_form.rb
@@ -4,11 +4,11 @@ class PinForm < Reform::Form
   property :user_id
   validates :user_id, presence: true
 
-  property :surgeon_id
-  validates :surgeon_id, presence: true
+  property :surgeon, form: SurgeonForm
+  validates :surgeon, presence: true
 
-  property :procedure_id
-  validates :procedure_id, presence: true
+  property :procedure, form: ProcedureForm
+  validates :procedure, presence: true
 
   property :cost
   property :sensation
@@ -31,10 +31,4 @@ class PinForm < Reform::Form
 
     property :caption
   end
-
-  # property :surgeon, form: SurgeonForm
-  # validates :surgeon, presence: true
-
-  # property :procedure, form: ProcedureForm
-  # validates :procedure, presence: true
 end
--- spec/models/pin_form_spec.rb
+++ spec/models/pin_form_spec.rb
@@ -1,37 +1,30 @@
 require "rails_helper"
 
 describe PinForm do
-  let(:pin) { build(:pin) }
+  let(:pin) { build(:pin, :with_surgeon_and_procedure) }
   let(:form) { PinForm.new(pin) }
-
-  def confirm_attributes(form, model)
-    model.attributes.each do |key, value|
-      case key
-      when 'updated_at', 'created_at', 'image_file_name',
-           'image_content_type', 'image_file_size',
-           'image_updated_at', 'username'
-        next
-      else expect(form.send(key)).to eq(value)
-      end
-    end
-  end
 
   it "should have all the attributes of a pin" do
     confirm_attributes(form, pin)
   end
 
-  context "with pin_images" do
+  context "inside pin_images" do
     it "should have all the attributes of a pin image" do
       confirm_attributes(pin.pin_images.first, pin.pin_images.first)
     end
+  end
 
-    it "should create the pin and associate its images when saved" do
+  context "with nested models" do
+    it "should create the pin and nested models when saved" do
       form.save
       saved_pin = form.model
       pin.pin_images.each do |pin_image|
         image = PinImage.find_by(caption: pin_image.caption)
         expect(image.pin).to eq(saved_pin)
       end
+
+      expect(Surgeon.where(email: pin.surgeon.email)).to exist
+      expect(Procedure.where(name: pin.procedure.name)).to exist
     end
   end
 end

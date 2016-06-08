058cd0630dbf04d96bc0e9636d293a290a5aee71
Cora Johnson-Roberson <me@corajr.com>
2016-06-07 21:28:24 -0400

    switch to reform form object

--- app/assets/javascripts/pins/new.js
+++ app/assets/javascripts/pins/new.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
   // dropzone setup
   var container = document.querySelector('#dropper'),
-  template = $('.hide').html(),
+  template = $('#preview-template').html(),
   queueCounter = -1;
   // TODO this should work but maybe load order prevents?
   Dropzone.autoDiscover = false;
   if (container && !!location.pathname.match(/pins\/new/)) {
-    var myDropzone = new Dropzone("#dropper", {
-      // must have slash to go out of pins/ namespace
-      // as pin_id wont exist yet
-      url: '/pin_images',
-      method: 'post',
+    $("#submit-all").prop("disabled", true);
+    var myDropzone = new Dropzone("#new_pin", {
+      previewsContainer: "#dropper",
+      clickable: "#dropper",
       maxFilesize: 1,
       previewTemplate: template,
       paramName: function(n) {
@@ -28,24 +27,45 @@ $(document).ready(function() {
         var submitButton = document.querySelector("#submit-all");
         myDropzone = this; // closure
 
-        submitButton.addEventListener("click", function() {
+        submitButton.addEventListener("click", function(event) {
+          event.preventDefault();
           myDropzone.processQueue(); // Tell Dropzone to process all queued files.
         });
       }
     });
+
     myDropzone.on("addedfile", function(file) {
+      $(".dz-message:visible").hide();
       $(".dz-preview:last-child").attr('id', "LM-" + file.lastModified);
       $('#submit-all').prop("disabled", false);
     });
+
+    myDropzone.on("removedfile", function() {
+      if ($(".dz-preview:visible").length == 0) {
+        $(".dz-message").show();
+        $('#submit-all').prop("disabled", true);
+      }
+    });
+
     myDropzone.on("sending", function(file, xhr, formData) {
       var captionEl = '#LM-' + file.lastModified + ' .caption';
       queueCounter += 1;
       formData.append('pin_images[' + queueCounter + ']caption', $(captionEl).val());
     });
-    myDropzone.on("success", function(file, responseText) {
-      var imageIdList = $('#pin_pin_image_ids');
-      // dynamically adding the save pin_image ids to the pin submission form
-      imageIdList.val(imageIdList.val() + "," + responseText.id);
+
+    myDropzone.on("sendingmultiple", function() {
+      tinyMCE.triggerSave();
+    });
+
+    myDropzone.on("successmultiple", function(file, responseText) {
+      window.location.href = "/pins/" + responseText.id;
+    });
+
+    myDropzone.on("errormultiple", function(file, errorMessage) {
+      $("#error_explanation ul").empty();
+      errorMessage.forEach(function(error) {
+        $("#error_explanation ul").append("<li>" + error + "</li>");
+      });
     });
   }
 });

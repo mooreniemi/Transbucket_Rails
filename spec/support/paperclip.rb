fee8dbb0a66b5cb8c8dece8bd29d499e9e60d7c3
Cora Johnson-Roberson <me@corajr.com>
2016-06-09 15:24:36 -0400

    clean paperclip after each

--- spec/support/paperclip.rb
+++ spec/support/paperclip.rb
@@ -1,7 +1,7 @@
 Paperclip::Attachment.default_options[:path] = "#{Rails.root}/spec/test_files/:class/:id_partition/:style.:extension"
 
 RSpec.configure do |config|
-  config.after(:suite) do
+  config.after(:each) do
     FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
   end
 end

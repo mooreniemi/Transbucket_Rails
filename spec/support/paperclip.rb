de3ec525f5bc09c928c329a2755c64a6277d966b
Cora Johnson-Roberson <me@corajr.com>
2016-06-09 15:03:19 -0400

    paperclip store test files in temp dir

---
+++ spec/support/paperclip.rb
@@ -0,0 +1,7 @@
+Paperclip::Attachment.default_options[:path] = "#{Rails.root}/spec/test_files/:class/:id_partition/:style.:extension"
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
+  end
+end

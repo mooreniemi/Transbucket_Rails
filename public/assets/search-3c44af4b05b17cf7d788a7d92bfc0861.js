3a7677991b3cf0a97c5160ace014e640d41f2787
Cora Johnson-Roberson <me@corajr.com>
2016-06-14 15:56:50 -0400

    recompiled assets

--- public/assets/search-3c44af4b05b17cf7d788a7d92bfc0861.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(function(){var e="/search_terms.json";$("#query").autocomplete({source:e,minLength:3,open:function(){setTimeout(function(){$(".ui-autocomplete").css("z-index",99999999999999)},0)}})})});

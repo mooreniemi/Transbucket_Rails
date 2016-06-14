be440f3e1dedc3f54d7d691545de74ff17f0fe76
Cora Johnson-Roberson <me@corajr.com>
2016-06-14 11:45:13 -0400

    normalize phone numbers in surgeon model

--- spec/models/surgeon_form_spec.rb
+++ spec/models/surgeon_form_spec.rb
@@ -2,9 +2,20 @@ require "rails_helper"
 
 describe SurgeonForm do
   let(:surgeon) { build(:surgeon) }
-  let(:form) { SurgeonForm.new(surgeon) }
 
   it "should have all the attributes of a surgeon" do
+    form = SurgeonForm.new(surgeon)
     confirm_attributes(form, surgeon)
+  end
+
+  it "should normalize the surgeon's phone number" do
+    form = SurgeonForm.new(Surgeon.new)
+
+    form.validate(surgeon.attributes)
+    form.save
+
+    surgeon_result = Surgeon.where(url: surgeon.url)
+    expect(surgeon_result).to exist
+    expect(surgeon_result.first.phone).to eq(surgeon.phone.phony_normalized(default_country_code: 'US'))
   end
 end

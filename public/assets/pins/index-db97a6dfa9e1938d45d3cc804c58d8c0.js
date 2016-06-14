f2a77509e1b4f0300b8a7b52f5a67bba0b04d084
Cora Johnson-Roberson <me@corajr.com>
2016-06-14 15:52:39 -0400

    unicorn config

---
+++ public/assets/pins/index-db97a6dfa9e1938d45d3cc804c58d8c0.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $("#clear-filter").click(function() {
+    $("#filter_dropdown select").val([]);
+    $("#filter_dropdown select").trigger('chosen:updated');
+  });
+
+  $("#scope").chosen({
+    width: "100%",
+    placeholder_text_multiple: "General tags"
+  });
+
+  $("#procedure").chosen({
+    width: "100%",
+    placeholder_text_multiple: "Procedures"
+  });
+
+  $("#surgeon").chosen({
+    width: "100%",
+    placeholder_text_multiple: "Surgeons"
+  });
+
+  // used to explain flagging
+  $('.label-with-popover').popover();
+
+  // masonry setup
+  var container = document.querySelector('#pins'),
+    msnry;
+
+  // TODO hack
+  if (container) {
+    // initialize Masonry after all images have loaded
+    imagesLoaded(container, function() {
+      msnry = new Masonry(container);
+    });
+  }
+});

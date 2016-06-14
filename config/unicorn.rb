7e5465c8dc454fa52f97778a988c3bffb24748cf
Cora Johnson-Roberson <me@corajr.com>
2016-06-14 14:37:08 -0400

    apache bench perf test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,18 @@
 # https://devcenter.heroku.com/articles/rails-unicorn
+# note that trapping signals can cause unicorn to hang on exit
+# see http://stackoverflow.com/a/20315864
 
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
+pid "tmp/pids/unicorn.pid"
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 end

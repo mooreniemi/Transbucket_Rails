a555bdc3b9477179de0672b56fdfa028427877a6
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-14 12:20:41 -0400

    issue 11, and some more coverage for surgeons

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,16 +1,16 @@
 // TODO switch to https://blog.twitter.com/2013/twitter-typeaheadjs-you-autocomplete-me
 
 $(document).ready(function() {
-    $(function() {
-        var realTime = '/search_terms.json';
-        $("#query").autocomplete({
-            source: realTime,
-            minLength: 3,
-            open: function() {
-                setTimeout(function() {
-                    $('.ui-autocomplete').css('z-index', 99999999999999);
-                }, 0);
-            }
-        });
+  $(function() {
+    var realTime = '/search_terms.json';
+    $("#query").autocomplete({
+      source: realTime,
+      minLength: 3,
+      open: function() {
+        setTimeout(function() {
+          $('.ui-autocomplete').css('z-index', 99999999999999);
+        }, 0);
+      }
     });
+  });
 });

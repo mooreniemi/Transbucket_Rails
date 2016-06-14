564a885a718a0b427a3730413751f9a70dedfa0e
Cora Johnson-Roberson <me@corajr.com>
2016-06-14 11:30:13 -0400

    change phone column to varchar(20)

--- db/schema.rb
+++ db/schema.rb
@@ -11,53 +11,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425182913) do
+ActiveRecord::Schema.define(version: 20160614152323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "assets", force: :cascade do |t|
-    t.string   "asset_file_name",    limit: 255
-    t.string   "asset_content_type", limit: 255
+    t.string   "asset_file_name"
+    t.string   "asset_content_type"
     t.integer  "asset_file_size"
     t.datetime "asset_updated_at"
     t.integer  "pin_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_file_name",    limit: 255
-    t.string   "image_content_type", limit: 255
+    t.string   "image_file_name"
+    t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "commentable_id",               default: 0
-    t.string   "commentable_type", limit: 255, default: ""
-    t.string   "title",            limit: 255, default: ""
-    t.text     "body",                         default: ""
-    t.string   "subject",          limit: 255, default: ""
-    t.integer  "user_id",                      default: 0,  null: false
+    t.integer  "commentable_id",   default: 0
+    t.string   "commentable_type", default: ""
+    t.string   "title",            default: ""
+    t.text     "body",             default: ""
+    t.string   "subject",          default: ""
+    t.integer  "user_id",          default: 0,  null: false
     t.integer  "parent_id"
     t.integer  "lft"
     t.integer  "rgt"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "state",            limit: 255
+    t.string   "state"
   end
 
   add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",               default: 0, null: false
-    t.integer  "attempts",               default: 0, null: false
-    t.text     "handler",                            null: false
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
     t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by",  limit: 255
-    t.string   "queue",      limit: 255
+    t.string   "locked_by"
+    t.string   "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20150425182913) do
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "genders", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -76,12 +76,12 @@ ActiveRecord::Schema.define(version: 20150425182913) do
   end
 
   create_table "pin_images", force: :cascade do |t|
-    t.string   "caption",            limit: 255
+    t.string   "caption"
     t.integer  "pin_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "photo_file_name",    limit: 255
-    t.string   "photo_content_type", limit: 255
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
   end
@@ -89,12 +89,12 @@ ActiveRecord::Schema.define(version: 20150425182913) do
   add_index "pin_images", ["pin_id"], name: "index_pin_images_on_pin_id", using: :btree
 
   create_table "pins", force: :cascade do |t|
-    t.string   "description",        limit: 255
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "image_file_name",    limit: 255
-    t.string   "image_content_type", limit: 255
+    t.string   "image_file_name"
+    t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "procedure_id"
@@ -102,8 +102,8 @@ ActiveRecord::Schema.define(version: 20150425182913) do
     t.integer  "surgeon_id"
     t.text     "details"
     t.integer  "cost"
-    t.string   "username",           limit: 255
-    t.string   "state",              limit: 255
+    t.string   "username"
+    t.string   "state"
     t.integer  "sensation"
     t.integer  "satisfaction"
   end
@@ -119,20 +119,20 @@ ActiveRecord::Schema.define(version: 20150425182913) do
   end
 
   create_table "procedures", force: :cascade do |t|
-    t.string   "name",             limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "body_type",        limit: 255
-    t.string   "gender",           limit: 255
+    t.string   "body_type"
+    t.string   "gender"
     t.integer  "avg_sensation"
     t.integer  "avg_satisfaction"
   end
 
   create_table "settings", force: :cascade do |t|
-    t.string   "var",         limit: 255, null: false
+    t.string   "var",         null: false
     t.text     "value"
-    t.integer  "target_id",               null: false
-    t.string   "target_type", limit: 255, null: false
+    t.integer  "target_id",   null: false
+    t.string   "target_type", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -147,28 +147,28 @@ ActiveRecord::Schema.define(version: 20150425182913) do
   end
 
   create_table "surgeons", force: :cascade do |t|
-    t.string   "first_name",     limit: 255
-    t.string   "address",        limit: 255
-    t.string   "city",           limit: 255
-    t.string   "state",          limit: 255
+    t.string   "first_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
     t.integer  "zip"
-    t.string   "country",        limit: 255
-    t.integer  "phone"
-    t.string   "email",          limit: 255
-    t.string   "url",            limit: 255
+    t.string   "country"
+    t.string   "phone",          limit: 20
+    t.string   "email"
+    t.string   "url"
     t.text     "procedure_list"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "last_name",      limit: 255
+    t.string   "last_name"
     t.text     "notes"
   end
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
-    t.string   "taggable_type", limit: 255
+    t.string   "taggable_type"
     t.integer  "tagger_id"
-    t.string   "tagger_type",   limit: 255
+    t.string   "tagger_type"
     t.string   "context",       limit: 128
     t.datetime "created_at"
   end
@@ -177,34 +177,34 @@ ActiveRecord::Schema.define(version: 20150425182913) do
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: :cascade do |t|
-    t.string  "name",           limit: 255
-    t.integer "taggings_count",             default: 0
+    t.string  "name"
+    t.integer "taggings_count", default: 0
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",    null: false
-    t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",                   limit: 255
-    t.string   "gender_id",              limit: 255
-    t.string   "username",               limit: 255
-    t.boolean  "admin",                              default: false
-    t.string   "md5",                    limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "name"
+    t.string   "gender_id"
+    t.string   "username"
+    t.boolean  "admin",                  default: false
+    t.string   "md5"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
@@ -213,11 +213,11 @@ ActiveRecord::Schema.define(version: 20150425182913) do
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
-    t.string   "votable_type", limit: 255
+    t.string   "votable_type"
     t.integer  "voter_id"
-    t.string   "voter_type",   limit: 255
+    t.string   "voter_type"
     t.boolean  "vote_flag"
-    t.string   "vote_scope",   limit: 255
+    t.string   "vote_scope"
     t.integer  "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"

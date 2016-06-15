9c97eec896e44e372e5c9e598496902bea21335b
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-14 23:54:40 -0400

    using rake db:seed to do all setup necessary

---
+++ db/seeds/procedures.rb
@@ -0,0 +1,18 @@
+Procedure.create!([
+  {name: "phalloplasty", body_type: nil, gender: "ftm", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "periareolar mastectomy (keyhole)", body_type: nil, gender: "ftm", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "double incision without grafts", body_type: nil, gender: "ftm", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "double incision with grafts", body_type: nil, gender: "ftm", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "metoidioplasty", body_type: nil, gender: "ftm", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "t anchor double incision", body_type: nil, gender: "ftm", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "vaginoplasty", body_type: nil, gender: "mtf", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "breast augmentation", body_type: nil, gender: "mtf", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "facial feminization surgery", body_type: nil, gender: "mtf", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "other", body_type: nil, gender: nil, avg_sensation: nil, avg_satisfaction: nil},
+  {name: "double incision with nipple retention", body_type: "Top", gender: "FTM", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "orchiectomy", body_type: "Bottom", gender: "MTF", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "mastectomy combined with liposuction", body_type: "Top", gender: "FTM", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "mansculpture", body_type: "Other", gender: "FTM", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "hysterectomy", body_type: "Bottom", gender: "FTM", avg_sensation: nil, avg_satisfaction: nil},
+  {name: "chest recontouring (liposuction)", body_type: "Top", gender: "FTM", avg_sensation: nil, avg_satisfaction: nil}
+])
---
+++ db/seeds/test_users.rb
@@ -0,0 +1,25 @@
+meow_meow = User.new(
+  email: 'bigbadmath@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  username: 'meowmeow',
+  name: 'AlexUser',
+  gender: Gender.first
+)
+meow_meow.skip_confirmation!
+meow_meow.save!
+puts "meowmeow user created successfully"
+
+zoon = User.new(
+  email: 'mooreniemi@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  username: 'zoon',
+  name: 'AlexAdmin',
+  gender: Gender.first,
+  admin: true
+)
+zoon.skip_confirmation!
+zoon.save!
+puts "zoon ADMIN created successfully"
+

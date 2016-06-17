db764b12065fc003f752ee3d5c70e93d4a9c4a5d
Cora Johnson-Roberson <me@corajr.com>
2016-06-17 16:14:09 -0400

    permit other attributes in update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update).push(:name, :username, :gender_id, :email)
+  end
+
   protected
 
   def after_inactive_sign_up_path_for(resource)
--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -22,34 +22,67 @@ RSpec.describe "user profile" do
     expect(find("#user_current_password").value).to eq(nil)
   end
 
-  it "updates the user's info on save" do
+  it "updates the user's profile info with a valid password" do
     within("#edit_user") do
       fill_in "user_name", :with => updated.name
       fill_in "user_username", :with => updated.username
       select updated.gender.name, :from => "user_gender_id"
-      fill_in "user_email", :with => updated.email
 
+      fill_in "user_current_password", :with => user.password
+      click_button "Update"
+    end
+
+    expect(page).to have_content("You updated your account successfully")
+
+    db_user = User.find(user.id)
+    expect(db_user.name).to eq(updated.name)
+    expect(db_user.username).to eq(updated.username)
+    expect(db_user.gender.id).to eq(updated.gender.id)
+  end
+
+  it "updates password if new password valid" do
+    within("#edit_user") do
       fill_in "user_password", :with => updated.password
       fill_in "user_password_confirmation", :with => updated.password
       fill_in "user_current_password", :with => user.password
       click_button "Update"
-
-      expect(find("#user_name").value).to eq(updated.name)
-      expect(find("#user_username").value).to eq(updated.username)
-      expect(find("#user_gender_id").value.to_i).to eq(updated.gender.id)
-      expect(find("#user_email").value).to eq(updated.email)
-
-      db_user = User.find(user.id)
-      expect(db_user.name).to eq(updated.name)
-      expect(db_user.username).to eq(updated.username)
-      expect(db_user.gender.id).to eq(updated.gender.id)
-      expect(db_user.valid_password?(updated.password)).to be true
     end
+
+    expect(page).to have_content("You updated your account successfully")
+
+    db_user = User.find(user.id)
+    expect(db_user.valid_password?(user.password)).to be false
+    expect(db_user.valid_password?(updated.password)).to be true
+  end
+
+  it "updates email only after reconfirming at the new address" do
+    clear_emails
+
+    within("#edit_user") do
+      fill_in "user_email", :with => updated.email
+      fill_in "user_current_password", :with => user.password
+      click_button "Update"
+    end
+
+    expect(page).to have_content("need to verify your new email address")
+
+    db_user = User.find(user.id)
+    expect(db_user.pending_reconfirmation?).to be true
+    expect(db_user.email).to eq(user.email)
+    expect(db_user.unconfirmed_email).to eq(updated.email)
+
+    open_email(updated.email)
+
+    current_email.click_link "Confirm my account"
+
+    db_user = User.find(user.id)
+    expect(db_user.email).to eq(updated.email)
   end
 
   it "displays errors on invalid input" do
     within("#edit_user") do
       fill_in "user_email", :with => "!%"
+      fill_in "user_current_password", :with => user.password
       click_button "Update"
     end
     expect(page).to have_content("Email is invalid")

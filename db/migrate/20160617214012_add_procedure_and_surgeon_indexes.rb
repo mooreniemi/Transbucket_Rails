0b1c331cb61e98d12655e5ac5a47a77c73fcd3fc
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-18 15:21:39 -0400

    reflecting name exactly

--- db/migrate/20160617214012_add_procedure_and_surgeon_indexes.rb
+++ db/migrate/20160617214012_add_procedure_and_surgeon_indexes.rb
@@ -1,4 +1,4 @@
-class ProcedureAndSurgeonIndexesOnPins < ActiveRecord::Migration
+class ProcedureAndSurgeonIndexes < ActiveRecord::Migration
   def change
     add_index :pins, :surgeon_id
     add_index :pins, :procedure_id

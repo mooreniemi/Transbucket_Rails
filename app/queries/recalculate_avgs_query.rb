e4384a243112231adc49c095f76a7c2f4cd8c92c
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-06-18 13:09:39 -0400

    average function

--- app/queries/recalculate_avgs_query.rb
+++ app/queries/recalculate_avgs_query.rb
@@ -4,10 +4,10 @@ class RecalculateAvgsQuery
     thing_of_type = type.find(id)
 
     return false if relevant_pins.empty?
-    new_avg_sat = relevant_pins.pluck(:satisfaction).reduce(0, :+) / relevant_pins.count
+    new_avg_sat = relevant_pins.average(:satisfaction)
 
     thing_of_type.avg_satisfaction = new_avg_sat
-    new_avg_sen = relevant_pins.pluck(:sensation).reduce(0, :+) / relevant_pins.count
+    new_avg_sen = relevant_pins.average(:sensation)
     thing_of_type.avg_sensation = new_avg_sen
 
     thing_of_type.save!

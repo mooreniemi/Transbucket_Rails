63f1361968f9ee29f932fbc45875b4df47513639
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-07-09 13:37:35 -0400

    complications and pin filter cleanup

--- spec/queries/pin_filter_query_spec.rb
+++ spec/queries/pin_filter_query_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 describe PinFilterQuery, "#filtered" do
-  let(:procedure) { create(:procedure, id: Constants::TOP_IDS[0]) }
-  let!(:pin) { create(:pin, procedure: procedure) }
-
-  it "handles nil just in case" do
-    expect(PinFilterQuery.new({scope: nil}).filtered).to eq(Pin.none)
+  before(:each) do
+    Rails.cache.clear
   end
-  it "takes a filter options hash and returns an active record relation" do
+
+  let(:procedure) { create(:procedure, id: Constants::TOP_IDS[0]) }
+  let!(:pin) do
+    pin = create(:pin, procedure: procedure)
+    pin.complication_list = "hematoma"
+    pin.save
+    pin.reload
+  end
+
+  it 'takes a filter options hash and returns an active record relation' do
     expect(PinFilterQuery.new({scope: ['top']}).filtered.first).to eq(pin)
   end
   it 'caches filters' do
@@ -17,7 +23,12 @@ describe PinFilterQuery, "#filtered" do
   end
 
   it 'handles different combinations of query' do
-    all_filters = {scope: 'top', surgeon: pin.surgeon.id.to_s, procedure: pin.procedure.id.to_s}
+    all_filters = {
+      scope: 'top',
+      surgeon: pin.surgeon.id.to_s,
+      procedure: pin.procedure.id.to_s,
+      complication: 'hematoma'
+    }
     (1..all_filters.size).each do |size|
       all_filters.keys.combination(size).each do |keys|
         filter = {}

343491e467e4ac56139af7381936d7fd6c991a00
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-07-12 22:39:07 -0400

    better cache indexes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -86,7 +86,7 @@ class Pin < ActiveRecord::Base
     timestamp = comment_threads.order('updated_at DESC').first.try(:updated_at)
     return [] if timestamp.nil?
 
-    Rails.cache.fetch("comments-from-#{timestamp.to_i}") do
+    Rails.cache.fetch("#{timestamp.to_i}-#{comment_threads.pluck(:id).join('-')}") do
       comment_threads.where(parent_id: nil).
         includes(:user).
         order('created_at desc')

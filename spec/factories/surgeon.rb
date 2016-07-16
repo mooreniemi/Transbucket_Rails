144c43b6dec418ed67ec3185218d3c9b52816966
Cora Johnson-Roberson <me@corajr.com>
2016-07-16 18:10:22 -0400

    remove apostrophes from surgeon factory

--- spec/factories/surgeon.rb
+++ spec/factories/surgeon.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :surgeon do
     first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
+    last_name { Faker::Name.last_name.gsub("'", "") }
     address { Faker::Address.street_address }
     city { Faker::Address.city }
     state { Faker::Address.state }

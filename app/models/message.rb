7d4a190771f779a6de1fbb06a231b5d48fb837d2
alex <bigbadmath@gmail.com>
2021-02-15 21:02:44 -0500

    don't validate @message.name, which will fix the contact page (wrt sends, flash still broken)

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,7 @@ class Message
 
   attr_accessor :name, :email, :subject, :body
 
-  validates :name, :email, :subject, :body, :presence => true
+  validates :email, :subject, :body, :presence => true
   validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => true
 
   def initialize(attributes = {})

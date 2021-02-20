c7f21f9d62cd2298c1497f4bd7f40d1acf14a4ca
alex <bigbadmath@gmail.com>
2021-02-20 15:05:22 -0500

    WIP commit, 15 rspec tests failing, but manually ads reform validator

--- app/forms/procedure_form.rb
+++ app/forms/procedure_form.rb
@@ -1,5 +1,5 @@
 class ProcedureForm < Reform::Form
-  property :name
+  property :name, validates: { presence: true, validate_unique_name: true }
   property :gender
   property :body_type
   property :avg_sensation

c6d6044d2ec1e9a4d4fab59468888a4a54b8285f
alex <bigbadmath@gmail.com>
2021-02-22 09:52:09 -0500

    Adding s3_host_alias to PinImage for CloudFront

--- app/models/pin_image.rb
+++ app/models/pin_image.rb
@@ -1,11 +1,16 @@
 # PinImages is probably somewhat confusing.
 # Probably should've just stuck paperclip on pin.
 class PinImage < ActiveRecord::Base
+  # NOTE: CDN to reduce data transfer cost from S3
+  CLOUDFRONT_URL = "https://dwusg3ww9j123.cloudfront.net/"
   belongs_to :pin
 
   has_attached_file :photo,
     styles: { medium: '320x240>', thumb: '100x100#' },
-    default_url: 'http://placekitten.com/200/300'
+    default_url: 'http://placekitten.com/200/300',
+    s3_host_alias: CLOUDFRONT_URL
+
   validates_attachment_content_type :photo,
     content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+
 end

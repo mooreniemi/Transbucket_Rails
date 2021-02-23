7eba5600bd26a67363e08209d37129776371bb66
alex <bigbadmath@gmail.com>
2021-02-22 21:27:56 -0500

    90-CloudFront - just stabbing in the dark now tbh

--- app/models/pin_image.rb
+++ app/models/pin_image.rb
@@ -5,9 +5,7 @@ class PinImage < ActiveRecord::Base
 
   has_attached_file :photo,
     styles: { medium: '320x240>', thumb: '100x100#' },
-    default_url: 'http://placekitten.com/200/300',
-    url: ':s3_alias_url',
-    s3_host_alias: 'https://dwusg3ww9j123.cloudfront.net/'
+    default_url: 'http://placekitten.com/200/300'
 
   validates_attachment_content_type :photo,
     content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']

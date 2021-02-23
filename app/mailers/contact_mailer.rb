887da4f8e026ea4fcf637081a707876e9784959d
alex <bigbadmath@gmail.com>
2021-02-22 23:27:55 -0500

    Fix contact page

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,14 @@
+# Because only admin@transbucket.com is verified on SendGrid
+# we use it for to and from
 class ContactMailer < ActionMailer::Base
-  default :to => "admin@transbucket.com"
+  ADMIN = "admin@transbucket.com"
+  default :to => ADMIN
 
   def new_message(message)
     @message = message
     mail(
-      :subject => "[Transbucket.com Support] #{message.subject}",
-      :from => message.email
+      :subject => "[Transbucket.com Support] #{message.subject} from #{message.email}",
+      :from => ADMIN
     )
   end
 end

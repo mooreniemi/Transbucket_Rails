af01dea777fd95bf60cad3ccddb939c8539ee5d8
alex <bigbadmath@gmail.com>
2021-04-05 23:58:42 -0400

    FlagFeedback - flags on Pins give feedback on submissions page and admin page

---
+++ app/assets/javascripts/pins/flags.js
@@ -0,0 +1,61 @@
+// FIXME: this is the bare minimum to give some feedback to users
+$(document).ready(function(){
+  // on index page
+  $(".flag-pin").each(function(){
+    $(this).unbind().on('ajax:success', function(e, data, status, xhr){
+      var pinId = $(this).attr('id').split("_")[0];
+
+      switch(data["status"]) {
+        case "voted_down":
+          // doesn't persist, but at least shows tapping it did something
+          $(this).replaceWith('<i class="fa fa-exclamation-circle" aria-hidden="true"></i>');
+          break;
+        case "removed":
+          // persists, item won't show up from server once in review
+          $('.item[data-pin-id="' + pinId + '"]').hide();
+          break;
+        default:
+          console.log("unknown status reached while flagging " + pinId);
+      }
+    }).on('ajax:error',function(e, xhr, status, error){
+      console.log("error = " + JSON.stringify(error));
+    });
+  });
+
+  // on admin page
+  $(".unflag-pin").each(function(){
+    $(this).unbind().on('ajax:success', function(e, data, status, xhr){
+      var pinId = $(this).attr('id').split("_")[0];
+
+      switch(data["status"]) {
+        case "unflagged":
+          // doesn't persist, but at least shows tapping it did something
+          $(this).replaceWith('<i class="fa fa-check" aria-hidden="true"></i>');
+          break;
+        default:
+          console.log("unknown status reached while unflagging " + pinId);
+      }
+    }).on('ajax:error',function(e, xhr, status, error){
+      console.log("e = " + JSON.stringify(e));
+      console.log("xhr = " + JSON.stringify(xhr));
+      console.log("status = " + JSON.stringify(status));
+      console.log("error = " + JSON.stringify(error));
+    });
+  });
+
+  $(".delete-pin").each(function(){
+    $(this).unbind().on('ajax:success', function(e, data, status, xhr){
+      var pinId = $(this).attr('id').split("_")[0];
+      switch(data["status"]) {
+        case "destroyed":
+          // persists, item won't show up from server once in review
+          $('.review-row[data-pin-id="' + pinId + '"]').hide();
+          break;
+        default:
+          console.log("unknown status reached while deleting " + pinId);
+      }
+    }).on('ajax:error',function(e, xhr, status, error){
+      console.log("error = " + JSON.stringify(error));
+    });
+  });
+});

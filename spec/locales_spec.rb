55f4d337b5e7f481e805fd1bda3d6f0ab91d3432
Alex Moore-Niemi <moore.niemi@gmail.com>
2026-09-09 08:39:10 -0400

    Complete account page localization

--- spec/locales_spec.rb
+++ spec/locales_spec.rb
@@ -5,6 +5,8 @@ describe 'application locales' do
   SUPPORTED_LOCALES = %w(en de es fr it ja zh-CN zh-TW pt-BR nl pl ru tr vi ar).freeze
   REQUIRED_KEYS = %w(site.description homepage.title homepage.intro header.search footer.discord_prefix locale.label legal.translation_notice account_menu.login filter_menu.apply filter_menu.clear filter_menu.scope filter_menu.procedure filter_menu.surgeon directory.procedures_title directory.procedures_intro directory.surgeons_title directory.surgeons_intro directory.submissions directory.submissions_intro directory.recent_submissions directory.search_results directory.search_description directory.name directory.average_satisfaction directory.average_sensation directory.discussion_threads directory.register_to_see_more newsfeed.title newsfeed.description newsfeed.date newsfeed.entries.procedure_cleanup newsfeed.entries.prefix_search newsfeed.entries.discord_invite newsfeed.entries.locales views.pagination.first views.pagination.last views.pagination.previous views.pagination.next views.pagination.truncate).freeze
   REQUIRED_FILTER_SCOPES = %w(ftm mtf bottom top need_category).freeze
+  REQUIRED_PROFILE_KEYS = %w(edit_title name email profile_help new_password current_password update submissions submit_now delete_confirm submission).freeze
+  REQUIRED_SETTINGS_KEYS = %w(title safe_mode safe_mode_help notifications notifications_help update cancel_account cancel_warning cancel_confirm).freeze
 
   def merge_translations(left, right)
     left.merge(right) do |_key, old_value, new_value|
@@ -27,6 +29,16 @@ describe 'application locales' do
       end
       REQUIRED_FILTER_SCOPES.each do |scope|
         value = @translations.fetch(locale).fetch('filter_scopes').fetch(scope)
+        expect(value).not_to be_nil
+        expect(value).not_to eq('')
+      end
+      REQUIRED_PROFILE_KEYS.each do |key|
+        value = @translations.fetch(locale).fetch('profile').fetch(key)
+        expect(value).not_to be_nil
+        expect(value).not_to eq('')
+      end
+      REQUIRED_SETTINGS_KEYS.each do |key|
+        value = @translations.fetch(locale).fetch('settings').fetch(key)
         expect(value).not_to be_nil
         expect(value).not_to eq('')
       end

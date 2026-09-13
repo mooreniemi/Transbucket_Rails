4b255be3bb56869026f38bd2401c123b78279f7d
Alex Moore-Niemi <moore.niemi@gmail.com>
2026-09-13 16:45:28 -0400

    Accept Rails route URL options argument

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -72,7 +72,7 @@ class ApplicationController < ActionController::Base
     redirect_to "#{localized_path}#{query}", status: :moved_permanently
   end
 
-  def default_url_options
+  def default_url_options(_options = nil)
     { locale: I18n.locale }
   end
 

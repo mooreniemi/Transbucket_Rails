3f80e35ab395c4e858abd30e234852119d2820ff
Alex Moore-Niemi <moore.niemi@gmail.com>
2026-09-12 22:15:45 -0400

    Improve pin ratings and layout

--- app/assets/javascripts/rated_divs.js
+++ app/assets/javascripts/rated_divs.js
@@ -2,23 +2,13 @@ $(document).ready(function() {
     $("#pin_sensation").rating({
         size: 'xs',
         step: 1,
-        starCaptions: {
-            1: "Very Poor",
-            2: "Poor",
-            3: "Ok",
-            4: "Good",
-            5: "Very Good"
-        }
+        showCaption: false,
+        showClear: false
     });
     $("#pin_satisfaction").rating({
         size: 'xs',
         step: 1,
-        starCaptions: {
-            1: "Very Unsatisfied",
-            2: "Unsatisfied",
-            3: "Currently unsure",
-            4: "Satisfied",
-            5: "Very Satisfied"
-        }
+        showCaption: false,
+        showClear: false
     });
 });

ec822714ccad2ca909cec3570070f95be4c1cfd6
Ali Areskog <write2ali@gmail.com>
2026-09-19 20:25:07 -0400

    add some index files

---
+++ react/src/components/header/index.ts
@@ -0,0 +1,4 @@
+export * from './header';
+export * from './headerLink';
+export * from './headerMenu';
+export * from './headerContext';
---
+++ react/src/components/table/index.ts
@@ -0,0 +1,6 @@
+export * from './cell';
+export * from './column';
+export * from './row';
+export * from './table';
+export * from './tableBody';
+export * from './tableHeader';
